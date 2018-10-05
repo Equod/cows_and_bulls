7278fee4bbdbf6aa2e6f70c2991a9b06135cd2fb
Illya Dudchenko <illya.dudchenko@gmail.com>
2018-10-05 21:34:41 +0200

    Moved generate_numbers.py from include to src

--- core/src/generate_numbers.py
+++ core/src/generate_numbers.py
@@ -4,13 +4,13 @@ def is_valid(number) :
     num_list = [(number // 1000) % 10, (number // 100) % 10, (number // 10) % 10, number % 10]
     return number > 1000 and len(num_list) == len(set(num_list))
 
-if not os.path.isfile('numbers.dat') :
+if not os.path.isfile('../include/numbers.dat') :
     list = []
     for n0 in range(1000, 9999) :
         if is_valid(n0) :
             list += [n0]
 
-    with open('numbers.dat', 'w') as file :
+    with open('../include/numbers.dat', 'w') as file :
         file.write(','.join(str(item) for item in list))
 
 

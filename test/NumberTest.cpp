f874667e82c1fc6c28b2262a0ff5c6de8d42c5cb
Illya Dudchenko <illya.dudchenko@gmail.com>
2018-10-10 01:28:06 +0200

    Adeded NumberResultTest

--- test/NumberTest.cpp
+++ test/NumberTest.cpp
@@ -9,13 +9,39 @@
 using cows_and_bulls::Number;
 using cows_and_bulls::Numbers;
 using cows_and_bulls::NumsTemplate;
+using cows_and_bulls::Result;
 
 class NumberTest : public ::testing::TestWithParam<Number> {};
 
-TEST_P(NumberTest, TestOk) {
+TEST_P(NumberTest, self_convertion_test) {
   auto num = GetParam();
   Number new_num(num);
   EXPECT_EQ(new_num, num);
 }
 
-INSTANTIATE_TEST_CASE_P(GetAsSingleNumTest, NumberTest, ::testing::ValuesIn(Numbers::nums));
+TEST_P(NumberTest, get_result_for_self) {
+  auto num = GetParam();
+  cows_and_bulls::Result result{4,0};
+  EXPECT_EQ(result, num.GetResultFor(num));
+}
+
+INSTANTIATE_TEST_CASE_P(NumbersConstructionTest, NumberTest, ::testing::ValuesIn(Numbers::nums));
+
+class NumberResultTest : public ::testing::TestWithParam<std::tuple<Number, Number, Result>> {};
+
+TEST_P(NumberResultTest, number_result_test) {
+ auto [base, target, result] = GetParam();
+ EXPECT_EQ(result, base.GetResultFor(target));
+ EXPECT_EQ(result, target.GetResultFor(base));
+}
+
+INSTANTIATE_TEST_CASE_P(TestResult, NumberResultTest, ::testing::Values(
+    //              base          target        expectation
+    std::make_tuple(Number{1023}, Number{4567}, Result{0, 0}),
+    std::make_tuple(Number{1023}, Number{1456}, Result{1, 0}),
+    std::make_tuple(Number{1023}, Number{1045}, Result{2, 0}),
+    std::make_tuple(Number{1023}, Number{1025}, Result{3, 0}),
+    std::make_tuple(Number{1023}, Number{2310}, Result{0, 4}),
+    std::make_tuple(Number{1023}, Number{1032}, Result{2, 2}),
+    std::make_tuple(Number{1023}, Number{2031}, Result{1, 3})
+));

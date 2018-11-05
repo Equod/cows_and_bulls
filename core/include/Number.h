0ceece589ccdf7d43b1009c7845042179f284d42
Illya Dudchenko <illya.dudchenko@gmail.com>
2018-11-05 23:41:41 +0100

    Initialize Number::number

--- core/include/Number.h
+++ core/include/Number.h
@@ -13,13 +13,14 @@ namespace cows_and_bulls {
 
 class Number : public std::array<size_t, 4> {
  public:
-  constexpr Number(size_t num) noexcept
-    : array{ (num / 1000) % 10, (num / 100) % 10, (num / 10) % 10, num % 10 }
+  constexpr explicit Number(size_t num) noexcept
+    : array{ (num / 1000) % 10, (num / 100) % 10, (num / 10) % 10, num % 10 }, number{num}
   {}
   Result GetResultFor(const Number& other) const noexcept;
   size_t GetAsSingleNum() const noexcept { return number; }
   friend std::ostream& operator<<(std::ostream& os, const Number& number);
   bool IsValid() const noexcept;
+
  private:
   size_t number;
 };
